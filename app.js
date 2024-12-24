bba98403a6a5a997e102ac53e9fe6f3a52dcfe58
Eddan <edvin.runhellen1@gmail.com>
2024-12-24 10:08:13 +0100

    Added character length selector function

--- app.js
+++ app.js
@@ -10,25 +10,24 @@ copyPassword()
 
 
 
+
+
 let password = document.getElementById("generatedPassword")
 function generatePassword() {
+    const sliderValueElement = document.getElementById("sliderValue");
+    const sliderValue = Number(sliderValueElement.textContent);
     let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let passwordLength = 12;
     let password = "";
-    for (let i = 0; i < passwordLength; i++) {
+    for (let i = 0; i < sliderValue; i++) {
         let randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1)
     }
     document.getElementById("generatedPassword").textContent = password
 }
 
-
-
-
-
 function copyPassword() {
     let copy = document.getElementById("copyImage")
-    copyImage.addEventListener("click", copyPasswordToClipboard);
+    copy.addEventListener("click", copyPasswordToClipboard);
 }
 
 function copyPasswordToClipboard() {
